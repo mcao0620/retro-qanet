bd8914378a641d8c4de632bc1010fd72307b541d
Michael Cao <caom@stanford.edu>
2021-03-08 21:52:24 -0800

    split up models

--- models.py
+++ models.py
@@ -79,6 +79,92 @@ class BiDAF(nn.Module):
 
 
 
+class SketchyReader(nn.Module):
+
+    def __init__(self, word_vectors, char_vectors, hidden_size, drop_prob=0.):
+        super(RetroQANet, self).__init__()
+
+        self.emb = layers.Embedding(word_vectors=word_vectors,
+                                    char_vectors=char_vectors,
+                                    hidden_size=hidden_size,
+                                    drop_prob=drop_prob)
+
+        hidden_size *= 2    # update hidden size for other layers due to char embeddings
+
+        self.enc = None     # embedding encoder layer
+
+        self.att = None     # context-query attention layer
+
+        self.mod = None     # model layer
+
+        self.efv = None     # external front verifier
+
+        self.out = None     # output layer
+    
+
+    def forward(self, cw_idxs, qw_idxs, cc_idxs, qc_idxs):
+        c_mask = torch.zeros_like(cw_idxs) != cw_idxs
+        q_mask = torch.zeros_like(qw_idxs) != qw_idxs
+        c_len, q_len = c_mask.sum(-1), q_mask.sum(-1)
+
+        c_emb = self.emb(cw_idxs, cc_idxs)         # (batch_size, c_len, hidden_size)
+        q_emb = self.emb(qw_idxs, qc_idxs)         # (batch_size, q_len, hidden_size)
+
+
+        c_enc = self.enc(c_emb, c_len)    # (batch_size, c_len, 2 * hidden_size)
+        q_enc = self.enc(q_emb, q_len)    # (batch_size, q_len, 2 * hidden_size)
+
+        att = self.att(c_enc, q_enc,
+                       c_mask, q_mask)    # (batch_size, c_len, 8 * hidden_size)
+
+        mod = self.mod(att, c_len)        # (batch_size, c_len, 2 * hidden_size)
+
+
+
+
+class IntensiveReader(nn.Module):
+    
+    def __init__(self, word_vectors, char_vectors, hidden_size, drop_prob=0.):
+        super(RetroQANet, self).__init__()
+
+        self.emb = layers.Embedding(word_vectors=word_vectors,
+                                    char_vectors=char_vectors,
+                                    hidden_size=hidden_size,
+                                    drop_prob=drop_prob)
+
+        hidden_size *= 2    # update hidden size for other layers due to char embeddings
+
+        self.enc = None     # embedding encoder layer
+
+        self.att = None     # context-query attention layer
+
+        self.mod = None     # model layer
+
+        self.efv = None     # external front verifier
+
+        self.out = None     # output layer
+    
+
+    def forward(self, cw_idxs, qw_idxs, cc_idxs, qc_idxs):
+        c_mask = torch.zeros_like(cw_idxs) != cw_idxs
+        q_mask = torch.zeros_like(qw_idxs) != qw_idxs
+        c_len, q_len = c_mask.sum(-1), q_mask.sum(-1)
+
+        c_emb = self.emb(cw_idxs, cc_idxs)         # (batch_size, c_len, hidden_size)
+        q_emb = self.emb(qw_idxs, qc_idxs)         # (batch_size, q_len, hidden_size)
+
+
+        c_enc = self.enc(c_emb, c_len)    # (batch_size, c_len, 2 * hidden_size)
+        q_enc = self.enc(q_emb, q_len)    # (batch_size, q_len, 2 * hidden_size)
+
+        att = self.att(c_enc, q_enc,
+                       c_mask, q_mask)    # (batch_size, c_len, 8 * hidden_size)
+
+        mod = self.mod(att, c_len)        # (batch_size, c_len, 2 * hidden_size)
+
+
+
+
 class RetroQANet(nn.Module):
 
     """Retro-Reader over QANet
