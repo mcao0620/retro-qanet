5823afd931cb633c67b6d5b566008ec41829d08e
Isaac Cheruiyot <icykip@stanford.edu>
2021-03-13 15:16:49 -0800

    testing to see if sigmoid needs help

--- layers.py
+++ layers.py
@@ -401,7 +401,9 @@ class FV(nn.Module):
         #linear layer
         M_X = self.verify_linear(torch.cat((M_1, M_2, M_3), dim=-1))
         #produce logits
-        sq1 = masked_softmax(torch.squeeze(M_X), mask, dim=-1, log_softmax=True)
+        print(M_X)
+        sq1 = masked_sigmoid(torch.squeeze(M_X), mask, log_sigmoid=True)
+        print(sq1)
     
         y_i = torch.squeeze(sq1[:,0])
 
