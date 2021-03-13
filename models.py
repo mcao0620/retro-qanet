19b6169fad9c42edc7849cc913dc6432d6c5d850
Isaac Cheruiyot <icykip@stanford.edu>
2021-03-13 03:54:48 -0800

    h

--- models.py
+++ models.py
@@ -249,6 +249,7 @@ class RetroQANet(nn.Module):
 
         yi_s = self.sketchy(cw_idxs, qw_idxs, cc_idxs, qc_idxs)
         yi_i, log_p1, log_p2 = self.intensive(cw_idxs, qw_idxs, cc_idxs, qc_idxs)
+        print(yi_s, yi_i, log_p1, log_p2)
         out = self.RV_TAV(yi_s, yi_i, log_p1, log_p2)
 
         return out
