attention masking fixes

layers.py
@@ -303,11 +303,10 @@ class SelfAttentionBlock(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x, mask):
-        norm_out = self.norm(x)
+        norm_out = self.norm(x).permute(1,0,2)
         attn_output, attn_output_weights = self.self_attn_layer(
-            norm_out, norm_out, norm_out, attn_mask=mask)
-
-        return self.dropout(x + attn_output)
+            norm_out, norm_out, norm_out, key_padding_mask=~mask)
+        return self.dropout(x + attn_output.permute(1,0,2))
 
 
 class PositionalEncoding(nn.Module):

train_apart.py
@@ -142,8 +142,7 @@ def main(args):
                         cw_idxs, qw_idxs, cc_idxs, qc_idxs)
                     y_temp = 1 - yi 
                     yi = torch.cat((yi, y_temp), dim=-1)
-                    loss = args.alpha_1 *  F.nll_loss(yi, torch.where(y1 == 0, 0, 1).type_as(yi))
-                     + args.alpha_2 * (F.nll_loss(log_p1, y1) + F.nll_loss(log_p2, y2))
+                    loss = args.alpha_1 *  F.nll_loss(yi, torch.where(y1 == 0, 0, 1).type_as(yi)) + args.alpha_2 * (F.nll_loss(log_p1, y1) + F.nll_loss(log_p2, y2))
                 elif args.model_name == 'retro':
                     log_p1, log_p2 = model(cw_idxs, qw_idxs, cc_idxs, qc_idxs)
                     loss = F.nll_loss(log_p1, y1) + F.nll_loss(log_p2, y2)