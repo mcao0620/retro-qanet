652914261d8009873204f3cb8aeb1a31d7879dc1
Isaac Cheruiyot <icykip@stanford.edu>
2021-03-17 00:41:26 -0700

    data files needed

--- train_apart.py
+++ train_apart.py
@@ -146,7 +146,7 @@ def main(args):
                     weights = torch.ones_like(log_p1)
                     weights[:,0] = 1/(log_p1.shape[1])
                     nll_loss = nn.NLLLoss(weight=weights)
-                    loss = args.alpha_1 * bceLoss(yi, torch.where(y1 == 0, 0, 1).type(torch.FloatTensor)) + args.alpha_2 * (F.nll_loss(log_p1, y1) + F.nll_loss(log_p2, y2))
+                    loss = args.alpha_1 * bceLoss(yi, torch.where(y1 == 0, 0, 1).type(torch.FloatTensor)) + args.alpha_2 * (nll_loss(log_p1, y1) + nll_loss(log_p2, y2))
                 elif args.model_name == 'retro':
                     log_p1, log_p2 = model(cw_idxs, qw_idxs, cc_idxs, qc_idxs)
                     loss = F.nll_loss(log_p1, y1) + F.nll_loss(log_p2, y2)
