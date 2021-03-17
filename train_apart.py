7b3dea36cc8951fc1b7f133d5c5c1450dbc7a34f
Isaac Cheruiyot <icykip@stanford.edu>
2021-03-17 01:22:24 -0700

    weights aadded

--- train_apart.py
+++ train_apart.py
@@ -144,7 +144,7 @@ def main(args):
                         #print(torch.max(log_p1.exp(), dim=1)[0])
                         #$print(torch.max(log_p2.exp(), dim=1)[0])
                     weights = torch.ones(log_p1.shape[1])
-                    weights[0] = 2/(log_p1.shape[1])
+                    weights[0] = 4/(log_p1.shape[1])
                     nll_loss = nn.NLLLoss(weight=weights.to(device='cuda:0'))
                     loss = args.alpha_1 * bceLoss(yi, torch.where(y1 == 0, 0, 1).type(torch.FloatTensor)) + args.alpha_2 * (nll_loss(log_p1, y1) + nll_loss(log_p2, y2))
                 elif args.model_name == 'retro':
