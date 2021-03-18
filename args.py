d11c0ff9c3a222929f1cc6f1025326e205630a2b
Isaac Cheruiyot <icykip@stanford.edu>
2021-03-18 13:13:48 -0700

    fixed tester

--- args.py
+++ args.py
@@ -214,9 +214,9 @@ def get_test_args():
                         help='Name for submission file.')
 
     # Require load_path for test.py
-    args = parser.parse_args()
-    if not args.load_path:
-        raise argparse.ArgumentError('Missing required argument --load_path')
+    #args = parser.parse_args()
+    #if not args.load_path:
+    #    raise argparse.ArgumentError('Missing required argument --load_path')
 
     return args
 
