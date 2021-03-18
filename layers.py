5afcad68ed6708f4a873ee1e6126209bf37913ba
Isaac Cheruiyot <icykip@stanford.edu>
2021-03-18 10:51:57 -0700

    fixes for overall QANET

--- layers.py
+++ layers.py
@@ -728,7 +728,8 @@ class RV_TAV(nn.Module):
         pred_answerable = self.beta * intensive_prediction + \
             (1-self.beta) * sketchy_prediction
         # Calcultes how certain we are of intesives prediction
-        has = starts * ends .to(device='cuda:0')
+        has = has = torch.tensor([s_in[x, starts[x]] * e_in[x, ends[x]]
+                            for x in range(s_in.shape[0])]).to(device='cuda')
         null = (s_in[:, 0] * e_in[:, 0]).to(device='cuda:0')
         span_answerable = has - null
         print(span_answerable)
