472fac261c122ab48740aefc0ad6288445ef4204
Ubuntu <qanon@qanet-experiments.el20gjbqbv3ulpqwocyo3suz3e.bx.internal.cloudapp.net>
2021-03-18 23:01:23 +0000

    quick fixes

--- train_apart.py
+++ train_apart.py
@@ -157,7 +157,7 @@ def main(args):
                     gt_0 = torch.zeros(yi.shape[0]).to(device)
                     gt_1 = torch.ones(yi.shape[0]).to(device)
                     loss = args.alpha_1 * bceLoss(yi, torch.where(y1 == 0, gt_0, gt_1).type(
-                        torch.FloatTensor)) + args.alpha_2 * (F.nll_loss(log_p1, y1) + F.nll_loss(log_p2, y2))
+                        torch.cuda.FloatTensor)) + args.alpha_2 * (F.nll_loss(log_p1, y1) + F.nll_loss(log_p2, y2))
                     #loss = F.nll_loss(log_p1, y1) + F.nll_loss(log_p2, y2)
                 elif args.model_name == 'retro':
                     log_p1, log_p2 = model(cw_idxs, qw_idxs, cc_idxs, qc_idxs)
